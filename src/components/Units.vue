c6b55a858a8ce2b537b44777349542cc51c6c5ab
Joshua Castor <jcasto3@uic.edu>
2018-02-12 10:46:25 -0600

    Refactoed small unit card into its own component

--- src/components/Units.vue
+++ src/components/Units.vue
@@ -16,27 +16,23 @@
         </button>
       </div>
       <div class="ui cards" id="unit-list">
-        <div :class='getCardClass(getUnit(id).element)'
+        <small-unit-card
           v-for="id in unitIDs"
-          :key="id">
-          <div class="content left aligned">
-            <img class="floated mini ui image" :src='getImageURL(getUnit(id).id)'>
-            <div class="header">{{ getUnit(id).guide_id }}: {{ getUnit(id).name }}</div>
-            <div class="meta">
-              {{ getRarity(getUnit(id).rarity) }},
-              {{ getUnit(id).element }},
-              {{ getUnit(id).gender }}
-            </div>
-          </div>
-        </div>
+          :key="id"
+          :unitData='getUnit(id)'></small-unit-card>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import SmallUnitCard from '@/components/UnitsComponents/SmallUnitCard';
+
 export default {
   props: ['fullUnitData'],
+  components: {
+    'small-unit-card': SmallUnitCard,
+  },
   watch: {
     fullUnitData(newData) {
       if (newData !== undefined) {
@@ -55,32 +51,11 @@ export default {
   },
   data() {
     return {
-      elementColor: {
-        fire: 'red',
-        water: 'blue',
-        earth: 'green',
-        thunder: 'yellow',
-        light: 'gray',
-        dark: 'purple',
-      },
       unitIDs: [],
       doSortByUnitID: true,
     };
   },
   methods: {
-    /* eslint-disable */
-    getImageURL(id) {
-      return `http://dlc.bfglobal.gumi.sg/content/unit/img/unit_ills_thum_${id}.png`;
-      // return '';
-    },
-    getRarity(rarity) {
-      return (+rarity === 8) ? 'OE' : `${rarity}*`;
-    },
-    getCardClass(element) {
-      const classObject = { card: true };
-      classObject[this.elementColor[element]] = true;
-      return classObject;
-    },
     sorUnitsByGuideID() {
       this.unitIDs.sort((idA, idB) => +this.getUnit(idA).guide_id - +this.getUnit(idB).guide_id);
     },
@@ -89,7 +64,7 @@ export default {
     },
     getUnit(id) {
       return this.fullUnitData[id];
-    }
+    },
   },
 };
 </script>
---
+++ src/components/UnitsComponents/SmallUnitCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class='getCardClass(unitData.element)'>
+      <div class="content left aligned">
+        <img class="floated mini ui image" :src='getImageURL(unitData.id)'>
+        <div class="header">{{ unitData.guide_id }}: {{ unitData.name }}</div>
+        <div class="meta">
+          {{ getRarity(unitData.rarity) }},
+          {{ unitData.element }},
+          {{ unitData.gender }}
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['unitData'],
+  data() {
+    return {
+      elementColor: {
+        fire: 'red',
+        water: 'blue',
+        earth: 'green',
+        thunder: 'yellow',
+        light: 'gray',
+        dark: 'purple',
+      },
+    };
+  },
+  methods: {
+    getCardClass(unitElement) {
+      const classObject = { card: true };
+      classObject[this.elementColor[unitElement]] = true;
+      return classObject;
+    },
+    /* eslint-disable */
+    getImageURL(id) {
+      return `http://dlc.bfglobal.gumi.sg/content/unit/img/unit_ills_thum_${id}.png`;
+      // return '';
+    },
+    getRarity(rarity) {
+      return (+rarity === 8) ? 'OE' : `${rarity}*`;
+    },
+  },
+};
+</script>
