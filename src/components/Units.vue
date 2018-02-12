41169250dbcf1d67e7cef0b74565509fef97cfd6
Joshua Castor <jcasto3@uic.edu>
2018-02-12 10:33:47 -0600

    Add ability to sort by unit ID or guide ID

--- src/components/Units.vue
+++ src/components/Units.vue
@@ -2,17 +2,30 @@
   <div id="units-container">
     <div v-show="fullUnitData === undefined">Loading unit data</div>
     <div v-show="fullUnitData !== undefined" class="ui container">
+      <div class='ui buttons'>
+        <button :class="{ui: true, button: true, positive: doSortByUnitID}"
+          @click="doSortByUnitID = true"
+          >
+          Sort By Unit ID
+        </button>
+        <div class="or"></div>
+        <button :class="{ui: true, button: true, positive: !doSortByUnitID}"
+          @click="doSortByUnitID = false"
+          >
+          Sort By Guide ID
+        </button>
+      </div>
       <div class="ui cards" id="unit-list">
-        <div :class='getCardClass(unit.element)'
-          v-for="unit in fullUnitData"
-          :key="unit.id">
+        <div :class='getCardClass(getUnit(id).element)'
+          v-for="id in unitIDs"
+          :key="id">
           <div class="content left aligned">
-            <img class="floated mini ui image" :src='getImageURL(unit.id)'>
-            <div class="header">{{ unit.guide_id }}: {{ unit.name }}</div>
+            <img class="floated mini ui image" :src='getImageURL(getUnit(id).id)'>
+            <div class="header">{{ getUnit(id).guide_id }}: {{ getUnit(id).name }}</div>
             <div class="meta">
-              {{ getRarity(unit.rarity) }},
-              {{ unit.element }},
-              {{ unit.gender }}
+              {{ getRarity(getUnit(id).rarity) }},
+              {{ getUnit(id).element }},
+              {{ getUnit(id).gender }}
             </div>
           </div>
         </div>
@@ -24,6 +37,22 @@
 <script>
 export default {
   props: ['fullUnitData'],
+  watch: {
+    fullUnitData(newData) {
+      if (newData !== undefined) {
+        this.unitIDs = Object.keys(newData);
+        // eslint-disable-next-line
+        console.log(this.unitIDs);
+      }
+    },
+    doSortByUnitID(newValue) {
+      if (newValue) {
+        this.sortUnitsByUnitID();
+      } else {
+        this.sorUnitsByGuideID();
+      }
+    },
+  },
   data() {
     return {
       elementColor: {
@@ -34,6 +63,8 @@ export default {
         light: 'gray',
         dark: 'purple',
       },
+      unitIDs: [],
+      doSortByUnitID: true,
     };
   },
   methods: {
@@ -50,6 +81,15 @@ export default {
       classObject[this.elementColor[element]] = true;
       return classObject;
     },
+    sorUnitsByGuideID() {
+      this.unitIDs.sort((idA, idB) => +this.getUnit(idA).guide_id - +this.getUnit(idB).guide_id);
+    },
+    sortUnitsByUnitID() {
+      this.unitIDs.sort((idA, idB) => +idA - +idB);
+    },
+    getUnit(id) {
+      return this.fullUnitData[id];
+    }
   },
 };
 </script>
