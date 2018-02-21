53bf25567e728042c20db5570641c9eefb3f882a
Joshua Castor <jcasto3@uic.edu>
2018-02-20 23:43:53 -0600

    Top left button on settings page now redirects back to home

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,3 +1,11 @@
 <template>
   <div>Settings related to the Brave Frontier Multi Tool would go here.</div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    this.$emit('updateheader');
+  },
+};
+</script>
