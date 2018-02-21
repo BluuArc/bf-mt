90e6feef42ca967824a1f567e7ba23557d8467a2
Joshua Castor <jcasto3@uic.edu>
2018-02-21 07:40:12 -0600

    Remove everything unnecessary for prod

--- src/components/Squad.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <div>Squad Page. Some I hope to eventually put here
-    are a spark simulator and a squad buff analyzer</div>
-</template>
--- src/components/Summoner.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <div>Summoner Page. Content here that I plan to put here include
-      leveling tables/graphs/calculators and a summoner skilldex</div>
-</template>
