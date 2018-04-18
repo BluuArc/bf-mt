b57b77ce889bd00084cd3494bbbb7be91083d1c2
Joshua Castor <jcasto3@uic.edu>
2018-04-18 09:21:58 -0500

    Updated GL unit data

--- src/App.vue
+++ src/App.vue
@@ -222,7 +222,7 @@ export default {
         });
       }
 
-      appendScript(`${scJsHost}statcounter.com/counter/counter.js`);
+      appendScript(`${window.scJsHost}statcounter.com/counter/counter.js`);
     }
   },
 };
