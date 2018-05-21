33dda7be43b095f784e669b22921736a5afe9dee
Joshua Castor <jcasto3@uic.edu>
2018-05-21 12:39:22 -0500

    Added support for getting bb data with script

--- data-downloader.js
+++ data-downloader.js
@@ -53,94 +53,169 @@ function downloadMultipleFiles(urlArr = [], numConcurrent = 1) {
   });
 }
 
-async function getUnitData(servers = ['gl', 'eu', 'jp']) {
+async function getUnitDataForServer(server = 'gl') {
   const elements = ['fire', 'water', 'earth', 'thunder', 'light', 'dark'];
+  const data = {
+    info: {
+      file: 'info.json',
+      data: {}
+    },
+    evo_list: {
+      file: 'evo_list.json',
+      data: {}
+    },
+    feskills: {
+      file: 'feskills.json',
+      data: {}
+    },
+  };
 
-  servers.forEach(async (server) => {
-    const data = {
-      info: {
-        file: 'info.json',
-        data: {}
-      },
-      evo_list: {
-        file: 'evo_list.json',
-        data: {}
-      },
-      feskills: {
-        file: 'feskills.json',
-        data: {}
-      },
-    };
+  logger.info(`${server}: getting files`);
+  const unitUrls = [];
+  Object.keys(data).map(key => data[key].file)
+    .forEach(f => {
+      unitUrls.push(getUrl(server, f));
+    });
+  const downloadResult = await downloadMultipleFiles(unitUrls, unitUrls.length);
+  // convert to JSON if necessary
+  downloadResult.map(r => {
+    if (typeof r.data === "string") {
+      r.data = JSON.parse(r.data);
+    }
+    return r;
+  }).forEach(r => {
+    const [name, extension] = r.url.split('.');
+    data[name].data = r.data;
+  });
 
-    logger.info(`${server}: getting files`);
-    const unitUrls = [];
-    Object.keys(data).map(key => data[key].file)
-      .forEach(f => {
-        unitUrls.push(getUrl(server, f));
+  logger.debug('info', Object.keys(data.info.data));
+
+  // merge data together
+  logger.info(`${server}: merging files`);
+  const [info, evo_list, feskills] = [data.info.data, data.evo_list.data, data.feskills.data];
+
+  Object.keys(evo_list).forEach(id => {
+    const entry = evo_list[id];
+    const curUnit = info[id],
+      nextUnit = info[entry.evo.id.toString()];
+
+    if (!curUnit) {
+      logger.warn(`Can't add evo data to current unit ${id} as entry is missing`);
+      return;
+    } else if (!nextUnit) {
+      logger.warn(`Can't add evo data to next unit ${entry.evo.id} as entry is missing`);
+      return;
+    }
+
+    curUnit.evo_mats = entry.mats;
+    curUnit.next = nextUnit.id.toString();
+    nextUnit.prev = curUnit.id.toString();
+  });
+
+  Object.keys(feskills).forEach(id => {
+    const entry = feskills[id];
+    const unit = info[id];
+    if (!unit) {
+      logger.warn(`Can't add SP data to unit ${id} as entry is missing`);
+      return;
+    }
+    unit.feskills = entry.skills;
+  });
+
+  // save data into files separated by element
+  logger.info(`${server}: saving files`);
+  elements.forEach((element, index) => {
+    const fileData = {};
+    Object.keys(info)
+      .filter(id => info[id].element === element)
+      .forEach(id => {
+        fileData[id] = info[id];
       });
-    const downloadResult = await downloadMultipleFiles(unitUrls, unitUrls.length);
-    // convert to JSON if necessary
+
+    logger.debug(`fileData ${element}`, Object.keys(fileData).length);
+    const filename = `units-${server}-${index+1}.json`;
+    fs.writeFileSync(`${outputFolder}/${filename}`, JSON.stringify(fileData, null, 2), 'utf8');
+    logger.info(`${server}: saved ${filename}`);
+  });
+  return info;
+}
+
+async function getBurstDataForServer(server = 'gl', unitData = {}) {
+  let bbData = {};
+  logger.info(`${server}: getting files`);
+  if (server === 'eu') {
+    const downloadResult = await downloadMultipleFiles([getUrl(server, 'bbs.json')]);
+    bbData = downloadResult.map(r => {
+        if (typeof r.data === "string") {
+          r.data = JSON.parse(r.data);
+        }
+        return r;
+      })[0].data;
+  } else {
+    const files = [];
+    for (let i = 0; i <= 9; ++i) {
+      files.push(getUrl(server, `bbs_${i}.json`));
+    }
+    const downloadResult = await downloadMultipleFiles(files);
     downloadResult.map(r => {
       if (typeof r.data === "string") {
         r.data = JSON.parse(r.data);
       }
       return r;
     }).forEach(r => {
-      const [name, extension] = r.url.split('.');
-      data[name].data = r.data;
+      const { data } = r;
+      Object.keys(data)
+        .forEach(key => {
+          bbData[key] = data[key];
+        })
     });
+  }
+  logger.debug('bbData', Object.keys(bbData));
 
-    logger.debug('info', Object.keys(data.info.data));
-    
-    // merge data together
-    logger.info(`${server}: merging files`);
-    const [info, evo_list, feskills] = [data.info.data, data.evo_list.data, data.feskills.data];
-
-    Object.keys(evo_list).forEach(id => {
-      const entry = evo_list[id];
-      const curUnit = info[id],
-        nextUnit = info[entry.evo.id.toString()];
-
-        if (!curUnit) {
-          logger.warn(`Can't add evo data to current unit ${id} as entry is missing`);
-          return;
-        }else if (!nextUnit) {
-          logger.warn(`Can't add evo data to next unit ${entry.evo.id} as entry is missing`);
-          return;
+  logger.info(`${server}: setting unit associations`);
+  const burstTypes = ['bb', 'sbb', 'ubb'];
+  Object.values(unitData)
+    .forEach(unit => {
+      burstTypes.forEach(type => {
+        if (unit[type]) {
+          const burstId = unit[type].id;
+          if (bbData[burstId]) {
+            if (!bbData[burstId].associated_units) {
+              bbData[burstId].associated_units = [];
+            }
+            bbData[burstId].associated_units.push(unit.id.toString());
+          } else {
+            logger.warn(`No burst ID ${burstId} found in data from unit ${type} ${unit.id} (${unit.name})`);
+          }
         }
-
-        curUnit.evo_mats = entry.mats;
-        curUnit.next = nextUnit.id.toString();
-        nextUnit.prev = curUnit.id.toString();
+      })
     });
 
-    Object.keys(feskills).forEach(id => {
-      const entry = feskills[id];
-      const unit = info[id];
-      if (!unit) {
-        logger.warn(`Can't add SP data to unit ${id} as entry is missing`);
-        return;
-      }
-      unit.feskills = entry.skills;
-    });
+  // split data into files based on first number of id
+  logger.info(`${server}: saving files`);
+  for (let i = 0; i <= 9; ++i) {
+    const fileData = {};
+    Object.keys(bbData)
+      .filter(id => +id[0] === i)
+      .forEach(id => {
+        fileData[id] = bbData[id];
+      });
 
-    // save data into files separated by element
-    logger.info(`${server}: saving files`);
-    elements.forEach((element, index) => {
-      const fileData = {};
-      Object.keys(info)
-        .filter(id => info[id].element === element)
-        .forEach(id => { fileData[id] = info[id]; });
-
-      logger.debug(`fileData ${element}`, Object.keys(fileData).length);
-      const filename = `units-${server}-${index+1}.json`;
-      fs.writeFileSync(`${outputFolder}/${filename}`, JSON.stringify(fileData, null, 2), 'utf8');
-      logger.info(`${server}: saved ${filename}`);
-    })
-  });
-
-
-
+    logger.debug(`fileData ${i}`, Object.keys(fileData).length);
+    const filename = `bbs-${server}-${i}.json`;
+    fs.writeFileSync(`${outputFolder}/${filename}`, JSON.stringify(fileData, null, 2), 'utf8');
+    logger.info(`${server}: saved ${filename}`);
+  }
+  
+  return bbData;
 }
 
-getUnitData();
+async function getData(servers = ['gl', 'eu', 'jp']) {
+  for (const s of servers) {
+    const unitData = await getUnitDataForServer(s);
+    await getBurstDataForServer(s, unitData);
+  }
+}
+
+// getUnitData();
+getData(['gl']);
