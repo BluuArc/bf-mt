ec4e10412a0f195437921b768e97736821c9d24a
Joshua Castor <jcasto3@uic.edu>
2018-08-24 11:55:43 -0500

    Added rights/credits card and data disclaimer card to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,6 +27,28 @@
       <v-flex>
         <v-card raised>
           <v-card-title>
+            <h1 class="title">Rights/Credits</h1>
+          </v-card-title>
+          <v-card-text>
+            <p>Thanks to the following for providing parts used in this tool.
+            All images and code belong to their respective owners.
+            Unless otherwise specified, this tool is not directly affiliated with
+            any of the entities named below.</p>
+            <ul>
+              <li>Brave Frontier assets by Gumi/Alim</li>
+              <li><a rel="noopener" target="_blank" href="https://github.com/cheahjs/bravefrontier_data">Brave Frontier Datamine</a> by Deathmax</li>
+              <li><a rel="noopener" target="_blank" href="https://vuetifyjs.com/en/">Vuetify</a></li>
+              <li><a rel="noopener" target="_blank" href="https://vuejs.org/">Vue.js</a> by Evan You</li>
+              <li><a rel="noopener" target="_blank" href="http://dexie.org/">Dexie.js</a> by David Fahlander</li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex>
+        <v-card raised>
+          <v-card-title>
             <h1 class="title">Old Version</h1>
           </v-card-title>
           <v-card-text>
@@ -35,6 +57,26 @@
           <v-card-actions>
             <v-btn flat rel="noopener" href="https://bluuarc.github.io/bf-mt/semantic_build/" target="_blank">
               Semantic UI Version
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex>
+        <v-card raised>
+          <v-card-title>
+            <h1 class="title">Data Disclaimer</h1>
+          </v-card-title>
+          <v-card-text>
+            <span>This tool uses StatCounter and Google Analytics for traffic analytics. You can find out more about them below.</span>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn flat rel="noopener" href="https://statcounter.com/" target="_blank">
+              StatCounter
+            </v-btn>
+            <v-btn flat rel="noopener" href="https://marketingplatform.google.com/about/analytics/" target="_blank">
+              Google Analytics
             </v-btn>
           </v-card-actions>
         </v-card>
