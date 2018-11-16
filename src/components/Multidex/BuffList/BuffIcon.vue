15d1de660668923ba0594653155d1dcff4ed0405
Joshua Castor <jcasto3@uic.edu>
2018-11-16 08:42:51 -0600

    Added initial linking of icon keys to sprite sheets

--- src/components/Multidex/BuffList/BuffIcon.vue
+++ src/components/Multidex/BuffList/BuffIcon.vue
@@ -1,18 +1,18 @@
 <template>
   <svg
+    class="buff-icon"
     :width="displaySize" :height="displaySize"
     :viewBox="`0 0 ${iconSize} ${iconSize}`"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink">
     <title v-if="iconKey" v-text="iconKey"/>
-    <template v-if="false">
-      test
-      <!-- <image
-        :width="sheetWidth" :height="sheetHeight"
-        :xlink:href="src"
-        :href="src"
-        :transform="customTransform || defaultTransform"
-      /> -->
+    <template v-if="iconConfig">
+      <!-- iconKey is on one of the buff sheets -->
+      <image
+        :width="iconConfig.width" :height="iconConfig.height"
+        :xlink:href="iconConfig.src"
+        :transform="`translate(${iconConfig.coords})`"
+      />
     </template>
     <template v-else>
       <image
@@ -27,6 +27,14 @@
 </template>
 
 <script>
+import {
+  battleBuffIconKeys,
+  sgBattleBuffIconKeys,
+  customBuffIconKeys,
+  ailmentBuffIconKeys,
+} from '@/modules/constants';
+import logger from '@/modules/Logger';
+
 export default {
   props: {
     iconKey: {
@@ -41,6 +49,70 @@ export default {
   computed: {
     iconSize () {
       return 32;
+    },
+    battleBuffMetaData: () => ({
+      src: require('@/assets/buff-translation/battle/battle_buff_icon.png'),
+      width: 480,
+      height: 416,
+    }),
+    sgBattleBuffMetaData: () => ({
+      src: require('@/assets/buff-translation/battle/battle_buff_icon_sg.png'),
+      width: 480,
+      height: 192,
+    }),
+    ailmentBuffMetaData: () => ({
+      src: require('@/assets/buff-translation/battle/battle_bad_icon.png'),
+      width: 192,
+      height: 32,
+    }),
+    customBuffMetaData: () => ({
+      src: require('@/assets/buff-translation/battle/custom-icons.png'),
+      width: 480,
+      height: 416,
+    }),
+    iconConfig () {
+      let result;
+      try {
+        result = this.getIconConfigForKey(this.iconKey);
+      } catch (err) {
+        logger.error(err);
+      }
+      return result;
+    },
+  },
+  methods: {
+    getIconCoordinates (index = 0, rowLength = 15) {
+      const y = Math.floor(index / rowLength);
+      const x = index - (y * rowLength);
+      return [x, y].map(c => c * -this.iconSize).join(' ');
+    },
+    getIconConfigForKey (iconKey) {
+      let config = {};
+      if (battleBuffIconKeys.includes(iconKey)) {
+        config = {
+          ...this.battleBuffMetaData,
+          coords: this.getIconCoordinates(battleBuffIconKeys.indexOf(iconKey)),
+        };
+      } else if (sgBattleBuffIconKeys.includes(iconKey)) {
+        config = {
+          ...this.sgBattleBuffMetaData,
+          coords: this.getIconCoordinates(sgBattleBuffIconKeys.indexOf(iconKey)),
+        };
+      } else if (customBuffIconKeys.includes(iconKey)) {
+        config = {
+          ...this.customBuffMetaData,
+          coords: this.getIconCoordinates(customBuffIconKeys.indexOf(iconKey)),
+        };
+      }  else if (ailmentBuffIconKeys.includes(iconKey)) {
+        config = {
+          ...this.ailmentBuffMetaData,
+          coords: this.getIconCoordinates(ailmentBuffIconKeys.indexOf(iconKey)),
+        };
+      }
+
+      return Object.keys(config).length > 0
+        ? config
+        : null;
     },
   },
 };
