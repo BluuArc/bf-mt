16c2c93f9cf0622dbdb6e7a7d28bf9e147c3566d
Joshua Castor <jcasto3@uic.edu>
2019-01-15 07:28:01 -0600

    Updated dev config for HMR in workers (tentative)

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   baseUrl: './',
+  configureWebpack: config => {
+    if (process.env.NODE_ENV !== 'production') {
+      // custom output config to allow HMR on workers
+      // according to https://github.com/developit/workerize-loader/blob/85f956551cb9963eb2414ea19e82d615fe4aa79a/src/index.js#L56
+      config.output = config.output || {};
+      config.output.library = 'BFMT';
+      config.output.libraryExport = 'default';
+      config.output.libraryTarget = 'self';
+    }
+  },
   pluginOptions: {
     webpackBundleAnalyzer: {
       openAnalyzer: process.env.NODE_ENV === 'production',
