24c9745b4c9c4cf8511c477476dfda845f8e3b3e
Joshua Castor <joshua.castor@joshuacastor.me>
2019-01-25 18:41:35 -0600

    Removed old dexie-worker code and dependencies

--- src/store/instances/dexie-client.js
+++ src/store/instances/dexie-client.js
@@ -1,25 +1,5 @@
-// const PromiseWorker = require('promise-worker');
-// const worker = new Worker('static/js/dexie-worker.js');
-// const pw = new PromiseWorker(worker);
 // basically an adapter for old API using raw PromiseWorker
 import defaultClient, { makeTableInstance, makeMultidexTableInstance } from '@/modules/BfmtDatabase/client';
-
-const wrapper = Object.freeze({
-  // worker: pw,
-  worker: defaultClient,
-  put: (table, entry) => defaultClient.put(table, entry),
-  get: (table, entry) => defaultClient.get(table, entry),
-  getFieldInEntry: (table, query, field) => defaultClient.getFieldInEntry(table, query, field),
-  getFieldKeys: (table, query, field) => defaultClient.getFieldKeys(table, query, field),
-  getById: (table, query, field, id) => defaultClient.getById({ table, query, field, id }),
-  getByIds: (table, query, field, ids, extractedFields) => defaultClient.getByIds({ table, query, field, ids, extractedFields }),
-  // put: (table, entry) => pw.postMessage({ command: 'put', args: [table, entry] }),
-  // get: (table, query) => pw.postMessage({ command: 'get', args: [table, query] }),
-  // getFieldInEntry: (table, query, field) => pw.postMessage({ command: 'getFieldInEntry', args: [table, query, field] }),
-  // getFieldKeyLength: (table, query, field) => pw.postMessage({ command: 'getFieldKeyLength', args: [table, query, field] }),
-  // getFieldKeys: (table, query, field) => pw.postMessage({ command: 'getFieldKeys', args: [table, query, field] }),
-  // getById: (table, query, field, id) => pw.postMessage({ command: 'getById', args: [table, query, field, id] }),
-});
 
 export function makeWorker (table) {
   const instance = makeTableInstance(table);
@@ -34,20 +14,6 @@ export function makeWorker (table) {
     getDbStats: (query) => instance.getDbStats(query),
   });
 }
-
-// export const makeWorker = (table) => Object.freeze({
-//   worker: pw,
-//   put: entry => wrapper.put(table, entry),
-//   get: query => wrapper.get(table, query),
-//   getFieldInEntry: (query, field) => wrapper.getFieldInEntry(table, query, field),
-//   getFieldKeyLength: (query, field) => wrapper.getFieldKeyLength(table, query, field),
-//   getFieldKeys: (query, field) => wrapper.getFieldKeys(table, query, field),
-//   getById: (query, field, id) => wrapper.getById(table, query, field, id),
-//   getDbStats: (query) => pw.postMessage({
-//     command: 'getDbStats',
-//     args: [table, query],
-//   }),
-// });
 
 export function makeMultidexWorker (moduleName) {
   const instance = makeMultidexTableInstance(moduleName);
@@ -67,13 +33,4 @@ export function makeMultidexWorker (moduleName) {
   });
 }
 
-// export function makeMultidexWorker (moduleName) {
-//   const miniDbCommand = `getMiniDb${moduleName[0].toUpperCase().concat(moduleName.slice(1))}`;
-//   return {
-//     ...makeWorker(moduleName),
-//     getById: (server, id) => wrapper.getById(moduleName, { server }, 'data', id),
-//     getMiniDb: (server, searchQuery = {}, miniDbFields = []) => wrapper.worker.postMessage({ command: miniDbCommand, args: [server, searchQuery, miniDbFields] }),
-//   };
-// }
-
-export default wrapper;
+export default defaultClient;
