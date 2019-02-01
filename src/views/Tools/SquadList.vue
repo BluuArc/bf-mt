c10774c4fc54c02c48022a42190f1d962d54747c
Joshua Castor <joshua.castor@joshuacastor.me>
2019-02-01 08:22:36 -0600

    Added sample squad for UI testing; added initial squad card

--- src/views/Tools/SquadList.vue
+++ src/views/Tools/SquadList.vue
@@ -1,3 +1,52 @@
 <template>
-  <h1>This is the squad list.</h1>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex>
+        <v-card class="pa-2">
+          <v-layout row>
+            <v-flex>
+              <h1 class="title">
+                {{ sampleSquad.name }}
+              </h1>
+            </v-flex>
+            <v-spacer/>
+            <v-flex style="flex-grow: 0;">
+              <h2 class="subheading">
+                Squad cost
+              </h2>
+            </v-flex>
+          </v-layout>
+          <v-layout row wrap>
+            <v-flex
+              v-for="(unit, i) in sampleSquad.units"
+              :key="[JSON.stringify(unit), i].join('-')"
+              xs12 md6>
+              {{ unit }}
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  computed: {
+    sampleSquad: () => ({
+      id: Math.random().toString().split('.')[1],
+      name: 'Example Squad',
+      units: (new Array(6))
+        .fill(0)
+        .map((_, i) => ({
+          id: '10017',
+          es: null,
+          spheres: [],
+          sp: [],
+          bbOrder: i,
+          bbType: ['bb', 'sbb', 'ubb'][Math.floor(Math.random() * 3)],
+        })),
+    }),
+  },
+};
+</script>
