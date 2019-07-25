62074071b20e15fd18fdc3e26c936dd808b104d7
Joshua Castor <joshua.castor@joshuacastor.me>
2019-07-25 17:58:27 -0500

    Added initial layout for editing individual entries, replace block button with larger thinner button for add category

--- src/components/Tools/TierListCreator/CategoryConfig.vue
+++ src/components/Tools/TierListCreator/CategoryConfig.vue
@@ -2,21 +2,20 @@
   <ul class="category-order-config">
     <!-- overall config -->
     <li
-      v-for="(category, i) in categories"
-      :key="`${category.name}-${i}`"
+      v-for="(category, c) in categories"
+      :key="`${category.name}-${c}`"
       class="category-order-config--category-entry"
     >
       <div class="category-order-config--category-entry-header">
         <v-btn
           icon
-          :class="{ 'entry-expansion-icon': true, active: expandedEntries.includes(i) }"
-          @click="toggleExpandedEntry(i)"
+          :class="{ 'entry-expansion-icon': true, active: expandedEntries.includes(c) }"
+          @click="toggleExpandedEntry(c)"
         >
           <v-icon>keyboard_arrow_right</v-icon>
         </v-btn>
         <div class="category-order-config--category-entry-header-config">
           <v-text-field label="Category Name" :value="category.name"/>
-
           <v-layout row wrap>
             <v-flex>
               <label>
@@ -34,31 +33,52 @@
         </div>
         <v-spacer/>
         <div>
-          <v-btn block flat :disabled="i === 0">
+          <v-btn block flat :disabled="c === 0">
             <v-icon>keyboard_arrow_up</v-icon>
           </v-btn>
           <v-btn block flat>
             <v-icon>close</v-icon>
           </v-btn>
-          <v-btn block flat :disabled="i === value.length - 1">
+          <v-btn block flat :disabled="c === categories.length - 1">
             <v-icon>keyboard_arrow_down</v-icon>
           </v-btn>
         </div>
       </div>
-      <v-expansion-panel :value="expandedEntries.includes(i) ? 0 : -1">
+      <v-expansion-panel :value="expandedEntries.includes(c) ? 0 : -1">
         <v-expansion-panel-content>
-          {{ getEntriesForCategory(i) }}
+          <ul class="entry-order-config mx-2">
+            <li
+              v-for="(entry, e) in getEntriesForCategory(c)"
+              :key="`${entry.id || entry.name || entry.imgUrl}-${e}`"
+            >
+              <individual-entry-config :entry="entry" :entryIndex="e" :numCategories="getEntriesForCategory(c).length"/>
+            </li>
+            <li>
+              <v-btn flat large>
+                <v-icon>add</v-icon>
+                Add Entry
+              </v-btn>
+            </li>
+          </ul>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </li>
     <li>
-      <v-btn block>Add Category</v-btn>
+      <v-btn flat large>
+        <v-icon>add</v-icon>
+        Add Category
+      </v-btn>
     </li>
   </ul>
 </template>
 
 <script>
+import IndividualEntryConfig from './IndividualEntryConfig';
+
 export default {
+  components: {
+    IndividualEntryConfig,
+  },
   props: {
     value: {
       type: Object,
@@ -98,6 +118,11 @@ export default {
 </script>
 
 <style lang="less">
+.entry-order-config {
+  list-style-type: none;
+  padding-left: 0;
+}
+
 .category-order-config {
   list-style-type: none;
   padding-left: 0;
---
+++ src/components/Tools/TierListCreator/IndividualEntryConfig.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-layout align-center>
+    <img
+      :src="entry.base64Url || entry.imgUrl"
+      width="70"
+      style="max-height: 70px"
+      class="ma-2"
+    />
+    <v-layout column>
+      <v-flex>
+        <span class="subheading">
+          <b v-text="entry.name"/>
+        </span>
+      </v-flex>
+      <v-layout wrap align-baseline>
+        <v-flex>
+          <v-text-field label="Alternate Art ID" hide-details/>
+        </v-flex>
+        <v-btn flat>Apply</v-btn>
+      </v-layout>
+      <v-flex>
+        <v-btn outline block>
+          Move to Category
+        </v-btn>
+      </v-flex>
+    </v-layout>
+    <div>
+      <v-btn block flat :disabled="entryIndex === 0">
+        <v-icon>keyboard_arrow_up</v-icon>
+      </v-btn>
+      <v-btn block flat>
+        <v-icon>close</v-icon>
+      </v-btn>
+      <v-btn block flat :disabled="entryIndex === numCategories - 1">
+        <v-icon>keyboard_arrow_down</v-icon>
+      </v-btn>
+    </div>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    entry: {
+      type: Object,
+      required: true,
+    },
+    numCategories: {
+      type: Number,
+      default: 0,
+    },
+    entryIndex: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
