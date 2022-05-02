80f63bacface03988bb6a7d8e723422ff41c330e
BluuArc <joshua.castor@joshuacastor.me>
2022-05-01 23:51:42 -0500

    Pull data for GL from bfmt-data repo for dictionary

--- src/store/multidex/dictionary.js
+++ src/store/multidex/dictionary.js
@@ -4,6 +4,49 @@ import downloadWorker from '../instances/download-worker';
 import { createState, createMutations, createActions, createGetters } from './helper';
 import { getCacheBustingUrlParam } from '@/modules/utils';
 import SWorker from '@/assets/sww.min';
+import { getJson } from '@/modules/download-helper';
+
+/**
+ * @param {Object} arg0
+ * @param {Logger} arg0.logger
+ * @param {Function} arg0.commit
+ * @param {string} arg0.logPrefix
+ */
+ async function getDatabaseForGlobal({ logger, commit, logPrefix }) {
+  const pageDb = {};
+  const loadPromises = [];
+  const cacheBustingParam = getCacheBustingUrlParam();
+
+  const fullList = await getJson(`https://joshuacastor.me/bfmt-data/gl/dictionary/bfmt_update-stats.json?${cacheBustingParam}`);
+  const remainingKeys = Object.keys(fullList);
+  const totalKeyCount = remainingKeys.length;
+  let countFinished = 0;
+  logger.debug(`Need to fetch ${totalKeyCount} files`);
+  commit('setLoadingMessage', `${logPrefix} (${remainingKeys.length} files remaining)`);
+  const generateUrlForId = (id) => `https://joshuacastor.me/bfmt-data/gl/dictionary/${id}.json?${cacheBustingParam}`;
+  const fetchJsonWhileDataRemains = async () => {
+    const currentId = remainingKeys.shift();
+    if (currentId !== void 0) {
+      const data = await getJson(generateUrlForId(currentId));
+      Object.assign(pageDb, data.entries);
+
+      if ((++countFinished % 15) === 0) {
+        logger.debug('finished getting', currentId, remainingKeys.length, 'files remaining');
+        commit('setLoadingMessage', `${logPrefix} (${(countFinished / totalKeyCount * 100).toFixed(2)}% complete)`);
+      }
+      await fetchJsonWhileDataRemains();
+    }
+  };
+
+  // have no more than 10 concurrent fetches occurring simultaneously
+  for (let i = 0; i < 10; ++i) {
+    loadPromises.push(fetchJsonWhileDataRemains());
+  }
+
+  await Promise.all(loadPromises);
+  logger.debug(`finished getting ${Object.keys(pageDb).length} entries`);
+  return pageDb;
+}
 
 const logger = new Logger({ prefix: '[STORE/DICTIONARY]' });
 const dbWorker = makeMultidexWorker('dictionary');
@@ -116,24 +159,27 @@ export default {
         commit('setLoadingMessage', logPrefix);
         const tempLogger = new Logger({ prefix: `[STORE/DICTIONARY-${server.toUpperCase()}]` });
         try {
-          const pageDb = {};
-          const loadPromises = [];
-          let countFinished = 0;
-          for (let i = 0; i <= 9; ++i) {
-            const url = `${baseUrl}/dictionary-${server}-${i}.json?${cacheBustingParam}`;
-            loadPromises.push(downloadWorker
-              .postMessage('getJson', [url])
-              .then(tempData => {
-                Object.keys(tempData)
-                  .forEach(id => {
-                    pageDb[id] = tempData[id].en || '';
-                  });
-                tempLogger.debug('finished getting', url, 10 - (++countFinished), 'files remaining');
-                commit('setLoadingMessage', `${logPrefix} (${10 - countFinished} files remaining)`);
-              }));
-          }
+          let pageDb = {};
+          if (server === 'gl') {
+            pageDb = await getDatabaseForGlobal({ logger: tempLogger, commit, logPrefix });
+          } else {
+            const loadPromises = [];
+            let countFinished = 0;
+            for (let i = 0; i <= 9; ++i) {
+              const url = `${baseUrl}/dictionary-${server}-${i}.json?${cacheBustingParam}`;
+              loadPromises.push(getJson(url)
+                .then(tempData => {
+                  Object.keys(tempData)
+                    .forEach(id => {
+                      pageDb[id] = tempData[id].en || '';
+                    });
+                  tempLogger.debug('finished getting', url, 10 - (++countFinished), 'files remaining');
+                  commit('setLoadingMessage', `${logPrefix} (${10 - countFinished} files remaining)`);
+                }));
+            }
 
-          await Promise.all(loadPromises);
+            await Promise.all(loadPromises);
+          }
           tempLogger.debug('result key length', Object.keys(pageDb).length);
           commit('setLoadingMessage', `Storing data for ${server.toUpperCase()} server`);
           await dispatch('saveData', { data: pageDb, server });
